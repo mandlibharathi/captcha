62557df0f6a71e7fdcf87c38eb67f1e1ca8497dc
mandlibharathi <mandlibharathi@gmail.com>
2019-07-07 17:09:53 +0530

    add all

---
+++ server.js
@@ -0,0 +1,38 @@
+const express=require('express')
+const app=express();
+const bodyParser=require('body-parser');
+const request=require('request')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname +'/index.html')
+})
+app.post('/subscribe',(req,res)=>{
+    if(
+        req.body.captcha===undefined ||
+        req.body.captcha===''||
+        req.body.captcha===null
+    ){
+return res.json({"sucess":false,"msg":"please select captcha"})
+    }
+    //secrect key
+const secretkey="6LdVbawUAAAAAGQqU7lD7jz4eSsnp7GnqzyeONyy"
+//verify url
+
+const verifyUrl=`
+https://google.com/recaptcha/api/siteverify?secret=
+${secretkey}&responce=${req.body.captcha}&remoteip=${req.connection.remoteAddress}`
+
+request(verifyUrl,(err,response,body)=>{
+  body=JSON.parse(body)
+  console.log(body)
+  if(body.success !== undefined && !body.success){
+ return res.json({"sucess":false,"msg":"failed to captcha verified"})
+  }
+  return res.json({"sucess":true,"msg":"capta verified"})
+})
+})   
+
+app.listen(5000,()=>console.log('server is running ....'))
